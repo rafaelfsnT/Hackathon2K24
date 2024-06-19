8126cfab3de77558329be03ca6bdee93156e0fbc
rafaelsaN <rafaelf_santos08@outlook.com>
2024-06-19 11:59:16 -0300

    colocando imagem

--- paginas/home.php
+++ paginas/home.php
@@ -16,7 +16,8 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-custom ">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./paginas/home">Início</a>
+            <a class="navbar-brand" href="./paginas/home"><img src="./img/logo.png" alt="" class="navbar-logo"></a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
