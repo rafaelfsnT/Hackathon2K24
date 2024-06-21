b8d71034537eef342ffba3e7ce018a1945d0a599
rafaelfsnT <rafaelf_santos08@oultook.com>
2024-06-20 22:02:59 -0300

    alterando

--- paginas/agendamentos.php
+++ paginas/agendamentos.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Idoso para Vacina</title>
-    <link rel="stylesheet" href="./css/cadastro.css">
+    <link rel="stylesheet" href="./css/agendamentos.css">
     <script src="./js/cadastro.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
@@ -22,24 +22,18 @@
                 <label for="nome">Nome</label>
                 <input type="text" id="nome" name="nome" required>
             </div>   
+            
         <div class="form-group">
                 <label for="dataNascimento">Data</label>
                 <input type="date" id="dataNascimento" name="dataNascimento" required>
             </div>
-          
-            <div class="form-group">
-                <label for="hora">Hora</label>
-                <input type="time" id="hora" name="hora" required>
-            </div>
+        
            
-            <div class="form-group">
-                <label for="vacina">Vacina</label>
-                <input type="text" id="vacina" name="vacina" required>
-            </div>
+          
            
            
             <div class="form-buttons">
-                <button type="submit" onclick= "entrarHome()">Agendar</button>
+               <a href="./paginas/home"> <button type="submit">Agendar</button></a> 
             </div>
         </form>
     </div>
