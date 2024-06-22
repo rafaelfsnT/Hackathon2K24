c25500e6a85c30d5276e7fef30a0afef978ce14f
rafaelsaN <rafaelf_santos08@outlook.com>
2024-06-22 00:14:51 -0300

    estilizando para um melhor funcionamento

--- admin/paginas/agendamentos.php
+++ admin/paginas/agendamentos.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Idoso para Vacina</title>
+    <title>Cadastro de Idoso</title>
     <link rel="stylesheet" href="./cssPages/agendamentos.css">
     <script src="./jsPages/cadastro.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
