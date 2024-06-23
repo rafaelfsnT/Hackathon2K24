097f67f1bf2f991a1c0d79669850ccca679ed566
rafaelsaN <rafaelf_santos08@outlook.com>
2024-06-23 17:49:25 -0300

    fazendo alterações

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,8 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="icon" href="./img/icone.png">
     <base href="<?php echo "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"]; ?>">
 
     <!-- Bootst rap core JavaScript-->
